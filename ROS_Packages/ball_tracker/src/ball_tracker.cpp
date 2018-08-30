83ed33cfe64fb29cffe961c2eafafb04b7170c8f
Martin-Klimstein <martin@klimstein.net>
2018-08-30 22:31:35 +0200

    Make ball_tracker display video stream

--- ROS_Packages/ball_tracker/src/ball_tracker.cpp
+++ ROS_Packages/ball_tracker/src/ball_tracker.cpp
@@ -1,14 +1,36 @@
+// ROS includes
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+// OpenCV includes
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Ball Tracker";
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-    ROS_DEBUG("Image received.");
+    // Image conversion
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+    // Displaying image
+    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+    cv::waitKey(3);
 }
 
 int main(int argc, char **argv)
 {
+    cv::namedWindow(OPENCV_WINDOW);
     ros::init(argc, argv, "ball_tracker");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
